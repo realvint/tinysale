d445f00b704cf95881ba5d4f9770f509b7eb9ece
Victor Malukhin <victor.malukhin@gmail.com>
2023-09-25 18:04:39 +0300

    Add Product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_120047) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_145738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_120047) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
---
+++ spec/factories/products.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :product do
+    user { nil }
+    name { "MyString" }
+  end
+end
