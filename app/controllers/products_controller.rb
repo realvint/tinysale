84850faf21fb1462cdf9ed30c62c01357f82ff25
Victor Malukhin <victor.malukhin@gmail.com>
2023-09-28 14:04:49 +0300

    Add create Product feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,14 +2,33 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @products = Product.all
+  end
 
+  def show
+    @product = Product.friendly.find(params[:id])
   end
 
   def new
-
+    @product = current_user.products.build
   end
 
   def create
+    @product = Product.new(product_params.merge(user: current_user))
 
+    if @product.save
+      respond_to do |format|
+        format.html { redirect_to @product }
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params[:product].delete(:price) if params[:product][:price].to_f.zero?
+    params.require(:product).permit(:name, :price)
   end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,7 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe "Products", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Products', type: :request do
+  let(:user) { create(:user) }
+
+  before { sign_in user }
+
+  describe 'GET index' do
+    it 'returns http success' do
+      get products_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET new' do
+    it 'returns http success' do
+      get new_product_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST create' do
+    context 'valid params with price as string' do
+      it 'succeeds' do
+        expect do
+          post products_path, params: {
+            product: {
+              name: 'New product name',
+              price: 'Big Money'
+            }
+          }
+        end.to change(Product, :count).by(1)
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context 'valid params with price as number' do
+      it 'succeeds' do
+        expect do
+          post products_path, params: {
+            product: {
+              name: 'New product name',
+              price: '42'
+            }
+          }
+        end.to change(Product, :count).by(1)
+        expect(response).to have_http_status(:redirect)
+        expect(Product.last.price.to_s).to eq('42.00')
+      end
+    end
   end
 end
