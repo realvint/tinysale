248c91cc021fd6141f8a9621af252bdb15c8aa1a
Victor Malukhin <victor.malukhin@gmail.com>
2023-10-05 16:25:18 +0300

    Fix: Update input field styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def form_input
     'block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
-     focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+     focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 focus:outline-none'
   end
 
   def form_label
