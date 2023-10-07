937b1614ae3195ce67bf4e9ada51069c9396d54f
Victor Malukhin <victor.malukhin@gmail.com>
2023-10-07 21:37:56 +0300

    Render contents form page

--- app/helpers/sidenav_helper.rb
+++ app/helpers/sidenav_helper.rb
@@ -1,14 +1,14 @@
 module SidenavHelper
-  def link_active?(link_name)
-    if controller_name == link_name
+  def link_active?(link_name, second_name = nil)
+    if controller_name == link_name || controller_name == second_name
       'bg-gray-50 text-indigo-600'
     else
       'text-gray-700 hover:text-indigo-600 hover:bg-gray-50'
     end
   end
 
-  def svg_link_active?(link_name)
-    if controller_name == link_name
+  def svg_link_active?(link_name, second_name = nil)
+    if controller_name == link_name || controller_name == second_name
       'text-indigo-600'
     else
       'text-gray-400 group-hover:text-indigo-600'
