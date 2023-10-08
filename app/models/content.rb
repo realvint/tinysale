4da000b697b7a1be7f822246da806f7bbbf35767
Victor Malukhin <victor.malukhin@gmail.com>
2023-10-08 21:04:00 +0300

    Add validates to Content

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,4 +2,6 @@ class Content < ApplicationRecord
   belongs_to :product, optional: true
 
   has_one_attached :file
+
+  validates :name, presence: true
 end
--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Content, type: :model do
   it { is_expected.to belong_to(:product).optional }
+  it { is_expected.to validate_presence_of(:name) }
 end
