5c76f82fcc2d13737a170dec5896b026c6ff37eb
Victor Malukhin <victor.malukhin@gmail.com>
2023-10-19 14:13:18 +0300

    Styled listing out products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   before_action :set_product, only: %i[show edit update toggle_published]
 
   def index
-    @products = Product.all
+    @products = Product.order(:created_at)
   end
 
   def show; end
