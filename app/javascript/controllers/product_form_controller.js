1230ff9597e62fa64a1265d2d5f42b73419b4726
Victor Malukhin <victor.malukhin@gmail.com>
2023-10-28 02:45:10 +0300

    Attaching cover image to product with Active Storage

--- app/javascript/controllers/product_form_controller.js
+++ app/javascript/controllers/product_form_controller.js
@@ -4,7 +4,7 @@ const imageTypes = ['image/gif', 'image/jpeg', 'image/jpg', 'image/png']
 
 // Connects to data-controller="product-form"
 export default class extends Controller {
-  static targets = ['thumbnail', 'thumbnailInput']
+  static targets = ['thumbnail', 'thumbnailInput', 'coverInput', 'cover', 'coverUploadSection', 'coverSection']
 
   attachThumbnail(e) {
     e.preventDefault()
@@ -13,9 +13,30 @@ export default class extends Controller {
 
     if (!imageTypes.includes(file.type)) {
       alert('Attached file must be an image!')
+    } else {
+      this.thumbnailInputTarget.files = e.target.files
+      this.thumbnailTarget.src = URL.createObjectURL(file)
     }
+  }
 
-    this.thumbnailInputTarget.files = e.target.files
-    this.thumbnailTarget.src = URL.createObjectURL(file)
+  changeCover(e) {
+    e.preventDefault()
+
+    this.coverInputTarget.click()
+  }
+
+  attachCover(e) {
+    e.preventDefault()
+
+    const file = e.target.files[0]
+
+    if (!imageTypes.includes(file.type)) {
+      alert('Attached file must be an image!')
+    } else {
+      this.coverInputTarget.files = e.target.files
+      this.coverTarget.src = URL.createObjectURL(file)
+      this.coverUploadSectionTarget.classList.add('hidden')
+      this.coverSectionTarget.classList.remove('hidden')
+    }
   }
 }
--- app/models/product.rb
+++ app/models/product.rb
@@ -12,6 +12,7 @@ class Product < ApplicationRecord
   has_many :contents, dependent: :destroy
 
   has_one_attached :thumbnail, dependent: :purge
+  has_one_attached :cover, dependent: :purge
 
   validates :name, presence: true, uniqueness: { scope: :user_id, case_sensitive: false }
 
